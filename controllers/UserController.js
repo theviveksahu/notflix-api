725d83a1c72ed5aeae1f9f3a6577db66f149ef2c
Vivek Sahu <viveksahu23@gmail.com>
2024-01-27 22:27:32 +0530

    initial commit

---
+++ controllers/UserController.js
@@ -0,0 +1,41 @@
+const User = require("../models/UserModel");
+
+module.exports.addToLikedMovies = async (req, res) => {
+  try {
+    const { email, data } = req.body;
+    const user = await User.findOne({ email });
+    if (user) {
+      const { likedMovies } = user;
+      const moviesAlreadyLiked = likedMovies.find(({ id }) => id === data.id);
+      if (!moviesAlreadyLiked) {
+        await User.findByIdAndUpdate(
+          user._id,
+          {
+            likedMovies: [...user.likedMovies, data],
+          },
+          { new: true }
+        );
+      } else {
+        return res.json({ msg: "Movie already added to liked list" });
+      }
+    } else {
+      await User.create({ email, likedMovies: [data] });
+    }
+    return res.json({ msg: "Movie added successfully" });
+  } catch (error) {
+    return res.json({ msg: "Error adding movie" });
+  }
+};
+
+module.exports.getLikedMovies = async (req, res) => {
+  try {
+    const { email } = req.params;
+    const user = await User.findOne({ email });
+
+    if (user) {
+      return res.json({ msg: "success", movies: user.likedMovies });
+    } else return res.json({ msg: "User not found" });
+  } catch (error) {
+    return res.json({ msg: "Error fetching movies" });
+  }
+};
---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const userRoutes = require("./routes/UserRoutes");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/notflix")
+  .then(() => console.log("DB connected"));
+
+app.use("/api/user", userRoutes);
+app.listen(5000, console.log("server started"));
