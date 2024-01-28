b41fb5b858322097a848255a4ab7060cffbeceb4
Vivek Sahu <viveksahu23@gmail.com>
2024-01-28 21:10:47 +0530

    removed from saved list api

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -39,3 +39,28 @@ module.exports.getLikedMovies = async (req, res) => {
     return res.json({ msg: "Error fetching movies" });
   }
 };
+
+module.exports.removeFromLikedMovies = async (req, res) => {
+  try {
+    const { email, movieId } = req.body;
+    const user = await User.findOne({ email });
+    if (user) {
+      const movies = user.likedMovies;
+      const movieIndex = movies.findIndex(({ id }) => id === movieId);
+      if (!movieIndex) {
+        res.status(400).send({ msg: "Movie not found." });
+      }
+      movies.splice(movieIndex, 1);
+      await User.findByIdAndUpdate(
+        user._id,
+        {
+          likedMovies: movies,
+        },
+        { new: true }
+      );
+      return res.json({ msg: "Movie successfully removed.", movies });
+    } else return res.json({ msg: "User with given email not found." });
+  } catch (error) {
+    return res.json({ msg: "Error removing movie to the liked list" });
+  }
+};
